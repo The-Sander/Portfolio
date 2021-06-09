c43cbf67833d0495e6cd4eea5c42696d7a00ad14
SanderBackx1 <sander.backx123@gmail.com>
2021-06-09 15:59:29 +0200

    title styling

--- src/components/ProjectHero.vue
+++ src/components/ProjectHero.vue
@@ -4,7 +4,9 @@
       <div class="hero-content">
         <h4>{{ info }}</h4>
         <h1>
-          <span class="line">{{ title }}</span>
+          <span class="line" v-for="(line, i) in title.split(' ')" :key="i">{{
+            line
+          }}</span>
         </h1>
         <div class="thumb">
           <img :src="img" alt="" />
