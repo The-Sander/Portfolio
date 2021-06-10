68ca4c54ad2b79283c31000aa2f742058633105f
SanderBackx1 <sander.backx123@gmail.com>
2021-06-10 13:19:23 +0200

    typo fix

--- src/components/ProjectTemplate.vue
+++ src/components/ProjectTemplate.vue
@@ -20,7 +20,7 @@
       </section>
     </div>
     <div class="wrapper">
-      <section class="main-section" v-if="role[0].lenght > 0">
+      <section class="main-section" v-if="role[0].length > 0">
         <article class="section">
           <div class="heading">
             <h4>My role</h4>
